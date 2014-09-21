6fdf1044df6b958ac9725440f0bd673d5aaaa290
Christian Boman <christianbboman@gmail.com>
2014-09-20 18:54:15 -0600

    Sat Sep 20 18:54:15 MDT 2014

--- arch_install/arch_install.sh
+++ arch_install/arch_install.sh
@@ -5,54 +5,7 @@ if [ ! $1 ]; then
    echo "Hostname not given. Exiting."
    exit 1
 fi
-
-pacman -Syy
-pacman -S --noconfirm zsh vim
-
-setfont Lat2-Terminus16
-
-#generate the locale
-sed -i "s/#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/g" /etc/locale.gen
-locale-gen
-
-echo LANG=en_US.UTF-8 > /etc/locale.conf
-export LANG=en_US.UTF-8
-
-#set the console font
-echo FONT=Lat2-Terminus16 > /etc/vconsole.conf
-
-#set the time zone
-ln -s /usr/share/zoneinfo/America/Denver /etc/localtime
-
-#set system clock
-hwclock --systohc --utc
-
-#set the hostnames
-echo $1 > /etc/hostname
-vim /etc/hosts
-
-#enable network services
-systemctl enable systemd-resolved systemd-networkd
-ln -sf /run/systemd/resolve/resolv.conf /etc/resolv.conf
-
-#install and set bootloader
-pacman -S --noconfirm gummiboot
-gummiboot install
-
-#configure the repos
-cp etc/pacman.conf /etc/pacman.conf
-pacman-key -r 962dde58
-pacman-key --lsign-key 962dde58
-
-pacman -Syy
-
-#install the rest of the system
-pacman -S --needed --noconfirm $(cat native_packages.txt)
-pacman -S --noconfirm infinality-bundle ibfonts-meta-base infinality-bundle-multilib 
-
-################do user stuff#############################
-
-visudo #add me to sudoers
+echo "christian ALL=(ALL) ALL" >> /etc/sudoers #add me to sudoers
 passwd #set root passwd
 
 groupadd sambashare  #great sambshare group
@@ -78,7 +31,6 @@ cp -rv boot home etc /
 
 ###################set up the samba mount#####################
 mkdir /mnt/samba
-cat fstab | grep samba >> /etc/fstab
 
 ###################create initramfs and enable services#######
 mkinitcpio -p linux
