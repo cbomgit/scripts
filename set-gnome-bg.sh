58b53ae81e96843704c47d8d6a64aa1222fc54d8
Christian Boman <christianbboman@gmail.com>
2015-01-02 21:40:11 -0700

    Changed background wallpaper path to point to Seafile folder

--- set-gnome-bg.sh
+++ set-gnome-bg.sh
@@ -3,7 +3,7 @@
 export DISPLAY=:0
 export XAUTHORITY=~/.Xauthority
 
-    IMAGE=$(find ~/Dropbox/wallpapers -type f \( -name '*.jpg' -o -name '*.png' \) -print0 | shuf -n1 -z)
+    IMAGE=$(find ~/Seafile/Wallpapers/wallpapers -type f \( -name '*.jpg' -o -name '*.png' \) -print0 | shuf -n1 -z)
 
     gsettings set org.gnome.desktop.background picture-uri "file://$IMAGE"
     gsettings set org.gnome.desktop.screensaver picture-uri "file://$IMAGE"
